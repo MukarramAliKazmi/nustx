4ca96d9859beec84157a64d6d7c3220b7617b440
Syed Mukarram Ali Kazmi <syed@fancity.co>
2023-04-20 02:02:02 +0500

    feat(apps): Replace default apps with custom ones for different user roles

--- apps/teacher/src/pages/index.tsx
+++ apps/teacher/src/pages/index.tsx
@@ -23,12 +23,12 @@ export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
-        <title>Web - Turborepo Example</title>
+        <title>Teacher - Turborepo Example</title>
       </Head>
 
       <main className="mx-auto w-auto px-4 pt-16 pb-8 sm:pt-24 lg:px-8">
         <h1 className="mx-auto text-center text-6xl font-extrabold tracking-tight text-white sm:text-7xl lg:text-8xl xl:text-8xl">
-          Web
+          Teacher
           <span className="block bg-gradient-to-r from-brandred to-brandblue bg-clip-text text-transparent px-2">
             Turborepo Example
           </span>
