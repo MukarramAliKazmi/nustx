e8cfb46620bef7c1d640d4e6a6bd16e6e510ba08
Syed Mukarram Ali Kazmi <syed@fancity.co>
2023-04-23 22:27:52 +0500

    chore(ui): Export components from ui package

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -2,3 +2,13 @@
 import '@/styles.css'
 
 // components
+export * from '@/components/button'
+export * from '@/components/discipline-card'
+export * from '@/components/input'
+export * from '@/components/logout-button'
+export * from '@/components/sidebar'
+export * from '@/components/sidebar-backdrop'
+export * from '@/components/sidebar-item'
+export * from '@/components/toggle-sidebar-button'
+export * from '@/components/toggle-theme-button'
+export * from '@/components/topbar'
