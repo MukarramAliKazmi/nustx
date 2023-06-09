4aa65eabf50ff078df97aed70a7a80e2c8dd7967
Syed Mukarram Ali Kazmi <mukarram.ali.kazmi@gmail.com>
2023-06-09 22:33:01 +0500

    feat(ui): Implement layout component

---
+++ packages/ui/src/components/layout.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+
+import { Sidebar } from '@/components/sidebar'
+import { Topbar } from '@/components/topbar'
+
+export interface LayoutProps {
+  children: React.ReactNode
+}
+
+export const Layout: React.FC<LayoutProps> = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false)
+
+  return (
+    <div className="w-screen h-screen flex flex-col bg-white dark:bg-gray-900 transition-colors duration-500">
+      <Topbar
+        isSidebarOpen={isSidebarOpen}
+        handleIsSidebarOpen={() =>
+          isSidebarOpen ? setIsSidebarOpen(false) : setIsSidebarOpen(true)
+        }
+      />
+      <div className="w-full h-full flex grid-cols-[auto_auto] relative bg-inherit overflow-x-hidden">
+        <Sidebar
+          isSidebarOpen={isSidebarOpen}
+          handleIsSidebarOpen={() =>
+            isSidebarOpen ? setIsSidebarOpen(false) : setIsSidebarOpen(true)
+          }
+        />
+        <div className="w-full md:w-screen h-full relative overflow-y-auto p-4 md:p-6">
+          {children}
+        </div>
+      </div>
+    </div>
+  )
+}
