23b0726c59f99f711a183c484f6ce458d97fc33f
Syed Mukarram Ali Kazmi <mukarram.ali.kazmi@gmail.com>
2023-06-24 17:54:35 +0500

    feat(admin): Add new app directory

--- apps/admin/src/app/page.tsx
+++ apps/admin/src/app/page.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
-import { NextPage } from 'next'
-import Head from 'next/head'
+import { Metadata } from 'next'
 
-const Home: NextPage = () => (
+export const metadata: Metadata = {
+  title: 'Web - Turborepo Example',
+}
+
+const Home = () => (
   <div className="flex min-h-screen flex-col items-center justify-center py-2">
-    <Head>
-      <title>Admin</title>
-    </Head>
     <main className="mx-auto w-auto px-4 pt-16 pb-8 sm:pt-24 lg:px-8">
       <h1 className="mx-auto text-center text-6xl font-extrabold tracking-tight text-white sm:text-7xl lg:text-8xl xl:text-8xl">
-        Admin
+        Web
+        <span className="block bg-gradient-to-r from-brandred to-brandblue bg-clip-text text-transparent px-2">
+          Turborepo Example
+        </span>
       </h1>
     </main>
   </div>
